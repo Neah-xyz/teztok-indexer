d460d42b4b1fe90ac7605a48291061a12273b73b
Timothy <timothy0336@hotmail.com>
2023-03-22 16:55:19 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -21,14 +21,12 @@ echo \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install docker
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Manage Docker as a non-root user
 sudo groupadd -f docker
 sudo usermod -aG docker ubuntu
-# User should run `newgrp docker` to switch to the docker group
-newgrp docker 
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -38,3 +36,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 18
 nvm use 18
+
+# User should run `newgrp docker` to switch to the docker group
+newgrp docker 
