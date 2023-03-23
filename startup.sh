094a95d35cf5cb13fb030b342bfcb0278ae33607
Timothy <timothy0336@hotmail.com>
2023-03-23 15:48:09 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y \
     dirmngr \
     unzip \
 
-# Needed for pyenv
+# Needed to build some dependencies
 sudo NEEDRESTART_MODE=a apt-get install -y build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -32,21 +32,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plu
 # Manage Docker as a non-root user
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
-
-# Install python
-if [ ! -r ~/.pyenv/ ]
-then
-    curl https://pyenv.run | bash
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-    echo 'eval "$(pyenv init -)"' >> ~/.profile
-    exec "$SHELL"
-    pyenv install 3.10
-    pyenv global 3.10
-fi
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
